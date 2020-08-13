cad71902a6719007b494e2b960b9f9fd33bc3d85
Usama Subhani <reachusama@protonmail.com>
2020-08-13 21:55:04 +0500

    Render app and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import BottomNav from './components/BottomNav/BottomNav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <BottomNav />
     </div>
   );
 }
