5fb492bae83828293d665cc96341ba40574cc815
Usama Subhani <reachusama@protonmail.com>
2020-08-13 21:54:52 +0500

    Add app bar

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AppBar, Button, Typography, IconButton, Toolbar } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Navigation = () => {
+    return (
+        <>
+           <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start"  color="inherit" aria-label="menu">
+                <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" >
+                News
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+            </AppBar>  
+        </>
+    )
+}
+
+export default Navigation
