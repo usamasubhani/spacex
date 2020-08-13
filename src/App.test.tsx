c44698337580d88ce93a08a46526e99d4ec07116
Usama Subhani <reachusama@protonmail.com>
2020-08-13 21:54:34 +0500

    Add Navigation tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {shallow, ShallowWrapper } from 'enzyme'
 import App from './App';
+import Navigation from './components/Navigation/Navigation';
+import BottomNav from './components/BottomNav/BottomNav';
 
 describe('App', () => {
   let container : ShallowWrapper;
@@ -10,5 +12,13 @@ describe('App', () => {
   it('should render a <div />', () => {
     expect(container.find('div').length).toEqual(1);
   });
+
+  it('should render Nav bar component', () => {
+    expect(container.containsMatchingElement(<Navigation />)).toEqual(true)
+  });
+
+  it("Should render Bottom Navigation component", () => {
+    expect(container.containsMatchingElement(<BottomNav />)).toEqual(true)
+  })
 
 });
