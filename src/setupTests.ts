a5eb1102ef2dcae2502d1b62aac8a4ad0b00782d
Usama Subhani <reachusama@protonmail.com>
2020-08-14 16:19:33 +0500

    Fix Material UI warnings

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,3 +2,5 @@
 import Enzyme from 'enzyme';
 import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new ReactSixteenAdapter() });
+import React from "react" 
+React.useLayoutEffect = React.useEffect 
