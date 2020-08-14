826e45c07617da47b9f28bf4bfcccb13304aa880
Usama Subhani <reachusama@protonmail.com>
2020-08-14 16:23:52 +0500

    Implement dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import BottomNav from './components/BottomNav/BottomNav';
+import { ThemeProvider, createMuiTheme, Paper } from '@material-ui/core';
 
 function App() {
+
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  })
   return (
     <div className="App">
-      <Navigation />
-      <BottomNav />
+      <ThemeProvider theme={theme}>
+        <Paper>
+          <Navigation />
+          <BottomNav />
+        </Paper>
+      </ThemeProvider>
     </div>
   );
 }
